0bdf0891cd9f610e7e6898516b3ba43d51577f91
Tegop <Tegop@MSI.attlocal.net>
2021-03-15 20:57:46 -0500

    Added Controller, Velocity, and Command. Also tweaked GameScene and added my classes to Sprite ye

---
+++ src/cmpsc390softwareproject/Velocity.java
@@ -0,0 +1,59 @@
+package cmpsc390softwareproject;
+
+import javafx.scene.Node; 
+
+public class Velocity
+{    
+    private double dx, dy;
+    private Node node;    
+    
+    public Velocity(Node node)    
+    {        
+        dx = 0;        
+        dy = 0;        
+        this.node = node;    
+    }    
+    
+    public Velocity(Node node, double dx, double dy)    
+    {        
+        this.dx = dx;
+        this.dy = dy;        
+        this.node = node;    
+    }    
+    
+    public void setVelocity(Node node, double dx, double dy)    
+    {        
+        this.dx = dx;
+        this.dy = dy;        
+        this.node = node;    
+    }    
+    
+    public double getVelocityX()    
+    {        
+        return dx;
+    }        
+    
+    public void setVelocityX(double dx)    
+    {        
+        this.dx = dx;
+    }    
+    
+    public double getVelocityY()    
+    {        
+        return dy;    
+    }    
+    
+    public void setVelocityY(double dy)    
+    {        
+        this.dy = dy;
+    }    
+    
+    public void update(double dt)    
+    {        
+        double ticks = 60*dt;        
+        //add the velocity to the location        
+        this.node.setTranslateX(this.node.getTranslateX()+dx*ticks);        
+        this.node.setTranslateY(this.node.getTranslateY()+dy*ticks);    
+    }
+}
+
