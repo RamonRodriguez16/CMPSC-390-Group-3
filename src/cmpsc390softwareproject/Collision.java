efa7a01e739d0e8c033873fc22f5cb46c4f02c4b
Tegop <Tegop@MSI.attlocal.net>
2021-04-05 19:36:06 -0500

    Fixed the boarder clipping and adjusted the enemy.

--- src/cmpsc390softwareproject/Collision.java
+++ src/cmpsc390softwareproject/Collision.java
@@ -20,13 +20,13 @@ public class Collision
     void checkCollisionY(Node target)
     {
         if (this.node.getBoundsInParent().intersects(target.getBoundsInParent()))
-            V.setVelocityY(-V.getVelocityY());
+            V.setVelocityY(-V.getVelocityY() * 2.0);
     }
     
     void checkCollisionX(Node target) 
     {
         if (this.node.getBoundsInParent().intersects(target.getBoundsInParent()))
-            V.setVelocityX(-V.getVelocityX());
+            V.setVelocityX(-V.getVelocityX() * 2.0);
     }
     
     void checkCollision(Node target)
@@ -34,9 +34,9 @@ public class Collision
         if (this.node.getBoundsInParent().intersects(target.getBoundsInParent()))
         {
             if(this.node.getTranslateX() < target.getTranslateX()-(target.getScaleX() / 2))
-                V.setVelocityX(-V.getVelocityX());
+                V.setVelocityX(-V.getVelocityX() * 2.0);
             else if(this.node.getTranslateX() > target.getTranslateX()+(target.getScaleX() / 2))
-                V.setVelocityX(-V.getVelocityX());
+                V.setVelocityX(-V.getVelocityX() * 2.0);
         }
     }
 }
